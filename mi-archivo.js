b2a06078e770e9d924075fe86e7d180ff477ee20
Ruben Fallas <rfallasl@ucenfotec.ac.cr>
2025-12-07 20:46:15 -0600

    Este es un nuevo cambio aplicado

---
+++ mi-archivo.js
@@ -0,0 +1,16 @@
+class HolaMundo {
+    static main() {
+        console.log("¡Hola, mundo desde JavaScript!");
+        this.saludar("Ruben");
+        this.sumar(5, 3);
+    }
+
+    static saludar(nombre) {
+        console.log(`Hola, ${nombre}. ¡Bienvenido al ejercicio de GitHub!`);
+    }
+
+    static sumar(a, b) {
+        const resultado = a + b;
+        console.log(`La suma de ${a} + ${b} es: ${resultado}`);
+    }
+}
